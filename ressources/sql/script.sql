df31817f06a4697d0bc925138d7e3de43b087e50
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-03-05 11:05:42 +0100

    fix(sql): update timestamp fields in USERS, RENTALS, and MESSAGES tables to ensure they are NOT NULL and have default values for better data integrity and consistency

--- ressources/sql/script.sql
+++ ressources/sql/script.sql
@@ -4,30 +4,30 @@ CREATE TABLE `USERS` (
   `email` varchar(255),
   `name` varchar(255),
   `password` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `RENTALS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `surface` numeric,
-  `price` numeric,
+  `surface` decimal(10,2),
+  `price` decimal(10,2),
   `picture` varchar(255),
   `description` varchar(2000),
   `owner_id` integer NOT NULL,
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `MESSAGES` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `rental_id` integer,
   `user_id` integer,
   `message` varchar(2000),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
 
